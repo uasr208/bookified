455d5eb7df165aed8a173558a5e4a1ef5dbeeb41
uasr208 <arunsinghutkarsh10@gmail.com>
2026-05-04 17:23:33 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, IBM_Plex_Serif, Mona_Sans } from "next/font/google";
+import "./globals.css";
+import { Variable } from "lucide-react";
+
+const ibmPlexSerif = IBM_Plex_Serif({
+  variable: "--font-ibm-plex-serif",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  display: "swap",
+});
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "Bookified",
+  description:
+    "Transform your books into interactive AI conversation. Upload pdf and chat with your books using voice.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${ibmPlexSerif.variable} ${monaSans.variable} relative font-sans antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
